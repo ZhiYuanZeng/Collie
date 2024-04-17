999e03925136ca76493f82b59fc3c85af9ad2f03
zyzeng <zyzeng@example.com>
2024-04-17 20:32:51 +0800

    add argument for training/eval kv cache compression

--- examples/mem_perceiver/finetune_mem_perceive.py
+++ examples/mem_perceiver/finetune_mem_perceive.py
@@ -1,6 +1,3 @@
-"""
-一个使用CoLLie训练Moss的例子（使用LOMO优化器，开启ZeRO3）
-"""
 import sys
 sys.path.append('..')
 from transformers import AutoTokenizer
@@ -15,7 +12,7 @@ from collie.controller.trainer import Trainer
 from collie.controller.evaluator import EvaluatorForPerplexity, EvaluatorForGeneration
 
 from collie.models import LlamaForCausalLM
-from collie.models.mem_perceiver import MemPerceiver
+from collie.models.mem_perceiver import MemPerceiver, ParallelMemPerceiver, H2oPruner, SparseParallelPerceiver, StreamingLMPruner, RandomPruner
 
 from collie.utils.monitor import StepTimeMonitor, TGSMonitor, MemoryMonitor, LossMonitor, EvalMonitor
 from collie.metrics import DecodeMetric, PPLMetric
@@ -24,12 +21,13 @@ import datasets
 from datasets import Dataset
 import torch
 import os
+import argparse
 
-os.environ['MASTER_PORT'] = "12345"
 
 # 1. 设置路径
 # 1.1 预训练模型路径
-pretrained_model = "/remote-home/share/models/open_llama_3b_v2/"
+# pretrained_model = "/remote-home/share/models/open_llama_3b_v2/"
+pretrained_model = "/remote-home/share/personal/zyzeng/models/models--TinyLlama--TinyLlama-1.1B-Chat-v1.0"
 
 # 2. 设置配置
 # 2.1 加载配置
@@ -37,12 +35,13 @@ config = CollieConfig.from_pretrained(pretrained_model, trust_remote_code=True)
 config.tp_size = 1
 config.dp_size = 1
 config.pp_size = 1
-config.train_epochs = 1
+config.train_epochs = 100
 config.eval_per_n_steps = 0
 config.eval_per_n_epochs = 1 
-config.train_micro_batch_size = 1
+config.train_micro_batch_size = 4
 config.gradient_accumulation_steps = 1
-config.eval_batch_size = 1
+config.eval_batch_size = 10
+config.use_flash = False
 config.ds_config = {
         "fp16": {
             "enabled": True
@@ -57,13 +56,13 @@ config.ds_config = {
         #     }
         # }
 }
-config.checkpointing = False
+config.checkpointing = True
 
 chunk_size=512
 d_model=config.hidden_size
 d_query=config.hidden_size // 4
 d_ffn=config.hidden_size // 2
-num_heads=config.num_key_value_heads
+num_heads=config.num_attention_heads
 query_len=chunk_size // 8
 num_layers=config.num_hidden_layers
 
@@ -74,7 +73,9 @@ mem_perceiver_config = {
     "chunk_size": chunk_size,
     "query_len": query_len,
     "num_heads": num_heads,
-    "num_layers": num_layers
+    "num_layers": num_layers,
+    "num_sink_tokens": 16,
+    "temperature": 0.1
 }
 setattr(config, 'mem_perceiver_config', mem_perceiver_config) 
 
@@ -83,22 +84,58 @@ setattr(config, 'mem_perceiver_config', mem_perceiver_config)
 tokenizer = AutoTokenizer.from_pretrained(pretrained_model, trust_remote_code=True)
 
 # 4. 加载数据集
-train_dataset = [
+max_seq_len = 1024
+eval_set_size = 50
+raw_train_dataset = [
     {
-        'tokens': torch.tensor(sample['input_ids']),
-        'output': torch.tensor(sample['labels']),
-        'attention_mask': torch.tensor(sample['attention_mask']),
+        'tokens': torch.tensor(sample['input_ids'])[:max_seq_len],
+        'output': torch.tensor(sample['labels'])[:max_seq_len],
+        'attention_mask': torch.tensor(sample['attention_mask'])[:max_seq_len],
     } for sample in datasets.load_from_disk("/remote-home/share/personal/zyzeng/data/demo_1k/")
 ]
-train_dataset = CollieDatasetForTraining(train_dataset)
-eval_dataset = train_dataset[:10]
+print('example data: {}'.format(tokenizer.decode(raw_train_dataset[0]['tokens'])))
+train_dataset = CollieDatasetForTraining(raw_train_dataset[:-eval_set_size])
+eval_dataset = train_dataset[-eval_set_size:]
+print(f'training set size: {len(train_dataset)}, eval set size: {len(eval_dataset)}')
 
 # 5. 加载预训练模型
 model = LlamaForCausalLM.from_pretrained(pretrained_model, config=config)
-mem_perceiver = MemPerceiver.from_config(config, model)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--compress_type", type=str, choices=['parallel_fuse', 'pipeline_fuse', 'h2o', 'parallel_sparse', 'local_window', 'no_compress', 'streaming', 'random_prune'])
+parser.add_argument("--do_train", action='store_true')
+parser.add_argument("--do_eval", action='store_true')
+args = parser.parse_args()
+ 
+compress_type = args.compress_type
+if compress_type == 'parallel_fuse':
+    mem_perceiver = ParallelMemPerceiver.from_config(config, model)
+elif compress_type == 'pipeline_fuse':
+    mem_perceiver = MemPerceiver.from_config(config, model)
+elif compress_type == 'h2o':
+    mem_perceiver = H2oPruner.from_config(config, model)
+elif compress_type == 'streaming':
+    mem_perceiver = StreamingLMPruner.from_config(config, model)
+elif compress_type == 'random_prune':
+    mem_perceiver = RandomPruner.from_config(config, model) # no compress
+elif compress_type == 'parallel_sparse':
+    mem_perceiver = SparseParallelPerceiver.from_config(config, model)
+elif compress_type == 'local_window': # remove context
+    config.mem_perceiver_config['query_len'] = 0
+    mem_perceiver = H2oPruner.from_config(config, model)
+elif compress_type == 'no_compress':
+    mem_perceiver = model # no compress
+else:
+    raise NotImplementedError
+model_for_training = mem_perceiver
 
 # 6. 设置优化器
-optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
+optimizer = torch.optim.Adam(model_for_training.parameters(), lr=2e-5)
+# optimizer = Lomo(
+#     model_for_training,
+#     lr = 0.001,
+#     clip_grad_norm = 5.0
+# )
 
 
 # 7. 添加监视器
@@ -112,7 +149,7 @@ monitors = [
 
 # 8. 添加Evaluator
 evaluator_ppl = EvaluatorForPerplexity(
-    model = model,
+    model = model_for_training,
     config = config,
     dataset = eval_dataset,
     monitors = [
@@ -123,7 +160,7 @@ evaluator_ppl = EvaluatorForPerplexity(
     }
 )
 evaluator_decode = EvaluatorForGeneration(
-    model = model,
+    model = model_for_training,
     config = config,
     tokenizer = tokenizer,
     dataset = eval_dataset,
@@ -138,7 +175,7 @@ evaluator_decode = EvaluatorForGeneration(
 
 # 9. 实例化trainer
 trainer = Trainer(
-    model = model,
+    model = model_for_training,
     config = config,
     loss_fn = GPTLMLoss(-100),
     optimizer = optimizer,
@@ -146,9 +183,12 @@ trainer = Trainer(
     monitors = monitors,
     evaluators = [evaluator_ppl]
 )
-torch.autograd.detect_anomaly(True)
 
-# 10. 训练/验证
-trainer.train()
+if args.do_train:
+    # 10. 训练/验证
+    trainer.train()
+
+if args.do_eval:
+    trainer.eval()
 
 #  Command CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:29402 --nnodes=1 --nproc_per_node=4 finetune_moss_for_training.py
