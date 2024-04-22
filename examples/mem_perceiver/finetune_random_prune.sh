1f81d41d8f26e3564b7af4e4cbb2d59382566eb2
zyzeng <zyzeng@example.com>
2024-04-22 15:20:49 +0800

    add script of runing chunk prefix pruner and random pruner

--- examples/mem_perceiver/finetune_random_prune.sh
+++ examples/mem_perceiver/finetune_random_prune.sh
@@ -1,3 +1,3 @@
-export MASTER_PORT="12341"
+export MASTER_PORT="12345"
 python=/remote-home/zyzeng/miniconda3/envs/collie/bin/python
 srun -p moss --gres=gpu:1 -u -n1 ${python} /remote-home/zyzeng/collie/examples/mem_perceiver/finetune_mem_perceive.py --compress_type random_prune --do_eval
