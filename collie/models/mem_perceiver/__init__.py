e5dc30f40c139bc34a52e78f83ec1eaa349ccc04
zyzeng <zyzeng@example.com>
2024-04-29 17:01:52 +0800

    add fuser in __init__

--- collie/models/mem_perceiver/__init__.py
+++ collie/models/mem_perceiver/__init__.py
@@ -1,2 +1,3 @@
 from .model import MemPerceiver, PerceiverLayer, ParallelMemPerceiver, ParallelPerceiverLayer
 from .pruner import H2oPruner, SparseParallelPerceiver, StreamingLMPruner, RandomPruner, ChunkPrefix, ChunkPostfix, TovaPruner, AutoPruner
+from .fuser import SparseFuserPerceiver, AutoFuser
