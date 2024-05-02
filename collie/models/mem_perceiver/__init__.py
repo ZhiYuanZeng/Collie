f6d9cd1886c0f186eae1f10b8b85b75196ba92d3
zyzeng <zyzeng@example.com>
2024-05-02 23:16:59 +0800

    add the pruner type and memory type into __init__.py

--- collie/models/mem_perceiver/__init__.py
+++ collie/models/mem_perceiver/__init__.py
@@ -1,3 +1,3 @@
 from .model import MemPerceiver, PerceiverLayer, ParallelMemPerceiver, ParallelPerceiverLayer
-from .pruner import H2oPruner, SparseParallelPerceiver, StreamingLMPruner, RandomPruner, ChunkPrefix, ChunkPostfix, TovaPruner, AutoPruner
+from .pruner import H2oPruner, SparseParallelPerceiver, StreamingLMPruner, RandomPruner, ChunkPrefix, ChunkPostfix, TovaPruner, AutoPruner, PrunerType, MemoryType
 from .fuser import SparseFuserPerceiver, AutoFuser
