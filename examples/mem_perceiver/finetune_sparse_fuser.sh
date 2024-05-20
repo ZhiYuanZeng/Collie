22e53ecaeb3f5dadaaf00535131e68a76338bfbc
zyzeng <zyzeng@example.com>
2024-05-21 00:23:54 +0800

    modify/add scripts of finetuning/evaluating compressers

--- examples/mem_perceiver/finetune_sparse_fuser.sh
+++ examples/mem_perceiver/finetune_sparse_fuser.sh
@@ -1,14 +1,20 @@
 export MASTER_PORT="12347"
 python=/remote-home/zyzeng/miniconda3/envs/collie/bin/python
-srun -p moss --gres=gpu:4 -u -n4 ${python} /remote-home/zyzeng/collie/examples/mem_perceiver/finetune_mem_perceive.py \
---fuser_type sparse_fuser \
---memory_type Incremental_Chunk_Streaming_Dynamic_History \
---do_train \
---lr 0.00002 \
---llm_model llama2_7b \
---num_train_samples 15000 \
---num_eval_samples 128 \
---chunk_size 512 \
---query_len 64 \
---d_query 1024 \
---use_flash
+perceiver_path='ckpts/llmllama2_7b#fuserperceiver#memoryChunk_Streaming#lr2e-05#chunk512#temperature1.0/epoch_1-batch_3000'
+srun -p moss --gres=gpu:1 -u -n1 ${python} /remote-home/zyzeng/collie/examples/mem_perceiver/finetune_mem_perceive.py \
+    --fuser_type perceiver \
+    --memory_type Chunk_Streaming \
+    --do_eval \
+    --lr 0.00002 \
+    --llm_model llama2_7b \
+    --num_train_samples 15000 \
+    --num_eval_samples 128 \
+    --chunk_size 512 \
+    --query_len 64 \
+    --d_query 1024 \
+    --use_flash \
+    --num_gpus 8 \
+    --compressed_chunk_size 64 \
+    --temperature 1.0 \
+    --eval_every 500 \
+    --perceiver_path $perceiver_path
