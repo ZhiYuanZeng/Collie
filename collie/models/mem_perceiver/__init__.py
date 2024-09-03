fdda21ffef82648bbd1a0ed78e30f6650e2a6f32
zyzeng <zyzeng@example.com>
2024-09-03 15:47:10 +0800

    add llm_dict in __init__

--- collie/models/mem_perceiver/__init__.py
+++ collie/models/mem_perceiver/__init__.py
@@ -1,2 +1,3 @@
-from .pruner import AutoPruner, PrunerType, MemoryType, PrunerLoss, IncrementalType, LayerwiseIncrementalType
+from .pruner import AutoPruner, PrunerType, MemoryType, PrunerLoss, IncrementalType, LayerwiseIncrementalType, AutoMemory
 from .fuser import SparseFuserPerceiver, AutoFuser
+from .utils import llm_dict
